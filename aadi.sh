566dec0d19199d85842372e606457473ec28b4a9
AaadiSathe <96834038+AaadiSathe@users.noreply.github.com>
2024-08-01 09:18:53 +0530

    Update aadi.sh

--- aadi.sh
+++ aadi.sh
@@ -2,3 +2,4 @@ hello world
 this is added recently 
 work to done 
 made some change i am gong to kill everuone in this hotel 
+this is a joke man dont get this thing serious i am kidding or not figure it out if you wanted to live in this world 
